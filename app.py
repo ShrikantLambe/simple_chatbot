b11f501460b4bc141b5b4815d3d6a2aefe0ca8d5
Shrikant Lambe <shrikantlambe@Shrikants-MacBook-Air.local>
2026-02-15 00:21:42 -0800

    Fix Render deployment: add app.py entry point

---
+++ app.py
@@ -0,0 +1,7 @@
+"""Entry point for the Flask web chatbot application"""
+from web_chatbot import app
+
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
